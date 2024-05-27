0b7f096105e57c4c195d1f0372bf594e83d601ea
Off The Rip <89182229+OffTheRip0@users.noreply.github.com>
2024-05-28 09:34:30 +1200

    first commit
    basic movement made and environment imported from blender, still need to do textures and lighting.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MoveSmoothTime;
+    public float GravityStrength;
+    public float JumpStrength;
+    public float WalkSpeed;
+    private CharacterController Controller;
+    private Vector3 CurrentMoveVelocity;
+    private Vector3 MoveDampVelocity;
+    private Vector3 CurrentForceVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Controller = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = 0f;
+        float z = Input.GetAxisRaw("Vertical");
+        Vector3 PlayerInput = new Vector3(x, y, z);
+
+        if (PlayerInput.magnitude > 1f)
+        {
+            PlayerInput.Normalize();
+        }
+
+        Vector3 MoveVector = transform.TransformDirection(PlayerInput);
+
+        //Function
+        CurrentForceVelocity = Vector3.SmoothDamp(CurrentForceVelocity, MoveVector * WalkSpeed, ref MoveDampVelocity, MoveSmoothTime);
+        
+        Controller.Move(CurrentForceVelocity * Time.deltaTime);
+
+        Ray groundCheckRay = new Ray(transform. position, Vector3.down);
+        if (Physics.Raycast(groundCheckRay, 1.1f))
+        {
+            CurrentForceVelocity.y = -2f;
+
+            if(Input.GetKey(KeyCode.Space))
+            {
+                CurrentForceVelocity.y = JumpStrength;
+            }
+        }
+        else
+        {
+            CurrentForceVelocity.y -= GravityStrength * Time.deltaTime;
+
+        }
+        Controller.Move(CurrentForceVelocity * Time.deltaTime);
+    }
+}
---
+++ Assets/Trigger.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour, IInteractable
+    {
+        public void Interact(){
+            Debug.Log("test");
+        }
+    }
