89acda61ad6dda317286e1e8cb3dbb18bcae76dc
Off The Rip <89182229+OffTheRip0@users.noreply.github.com>
2024-07-23 10:10:43 +1200

    small changes with ui and level system

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class Trigger : MonoBehaviour, IInteractable
     {
+        string objectName = gameObject.name;
         public void Interact(){
             Debug.Log("test");
         }
+        
     }
---
+++ Assets/Scripts/mainLevelSysyem.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainLevelSysyem : MonoBehaviour
+{
+    public Timer mytimer;
+    public bool ak47 = false;
+    public bool glock18 = false;
+    public bool cash = false;
+    public bool laptop = false;
+    public bool ipad = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
