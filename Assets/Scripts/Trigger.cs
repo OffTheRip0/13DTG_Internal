885b32a10f5a63b7ff76841fdffa87a9ea1c04ce
Off The Rip <89182229+OffTheRip0@users.noreply.github.com>
2024-07-24 12:24:55 +1200

    made object find and interact system basics

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -1,14 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq.Expressions;
 using UnityEngine;
-using UnityEngine.Animations;
 
 public class Trigger : MonoBehaviour, IInteractable
     {
-        string objectName = gameObject.name;
-        public void Interact(){
-            Debug.Log("test");
-        }
+    string objectName;
+    void Awake()
+    {
+        objectName = gameObject.transform.name;
+    }
+    public void Interact()
+    {
+        Debug.Log(objectName + " has been clicked on.");
+    }
         
     }
