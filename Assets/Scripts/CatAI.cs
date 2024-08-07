a3b6b7bb79fb6a7904448b33415399e0ff483b00
Off The Rip <89182229+OffTheRip0@users.noreply.github.com>
2024-08-07 12:31:13 +1200

    added more things to find and edited the cat AI to attack by going to player

--- Assets/Scripts/CatAI.cs
+++ Assets/Scripts/CatAI.cs
@@ -1,30 +1,20 @@
-
 using UnityEngine;
 using UnityEngine.AI;
 
 public class CatAI : MonoBehaviour
 {
     public NavMeshAgent agent;
-
     public Transform player;
-
     public LayerMask whatIsGround, whatIsPlayer;
-
-    public float health;
 
     //Patroling
     public Vector3 walkPoint;
     bool walkPointSet;
     public float walkPointRange;
 
-    //Attacking
-    public float timeBetweenAttacks;
-    bool alreadyAttacked;
-    public GameObject projectile;
-
     //States
-    public float sightRange, attackRange;
-    public bool playerInSightRange, playerInAttackRange;
+    public float detectionRange;
+    public bool playerInRange;
 
     private void Awake()
     {
@@ -34,13 +24,11 @@ public class CatAI : MonoBehaviour
 
     private void Update()
     {
-        //Check for sight and attack range
-        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
-        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+        //Check for detection range
+        playerInRange = Physics.CheckSphere(transform.position, detectionRange, whatIsPlayer);
 
-        if (!playerInSightRange && !playerInAttackRange) Patroling();
-        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
-        if (playerInAttackRange && playerInSightRange) AttackPlayer();
+        if (!playerInRange) Patroling();
+        if (playerInRange) ChasePlayer();
     }
 
     private void Patroling()
@@ -56,6 +44,7 @@ public class CatAI : MonoBehaviour
         if (distanceToWalkPoint.magnitude < 1f)
             walkPointSet = false;
     }
+
     private void SearchWalkPoint()
     {
         //Calculate random point in range
@@ -73,46 +62,9 @@ public class CatAI : MonoBehaviour
         agent.SetDestination(player.position);
     }
 
-    private void AttackPlayer()
-    {
-        //Make sure enemy doesn't move
-        agent.SetDestination(transform.position);
-
-        transform.LookAt(player);
-
-        if (!alreadyAttacked)
-        {
-            ///Attack code here
-            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
-            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
-            rb.AddForce(transform.up * 8f, ForceMode.Impulse);
-            ///End of attack code
-
-            alreadyAttacked = true;
-            Invoke(nameof(ResetAttack), timeBetweenAttacks);
-        }
-    }
-    private void ResetAttack()
-    {
-        alreadyAttacked = false;
-    }
-
-    public void TakeDamage(int damage)
-    {
-        health -= damage;
-
-        if (health <= 0) Invoke(nameof(DestroyEnemy), 0.5f);
-    }
-    private void DestroyEnemy()
-    {
-        Destroy(gameObject);
-    }
-
     private void OnDrawGizmosSelected()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, attackRange);
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, sightRange);
+        Gizmos.DrawWireSphere(transform.position, detectionRange);
     }
 }
---
+++ Assets/Scripts/cursorSizeChanger.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class cursorSizeChanger : MonoBehaviour
+{
+    public float size { get; set; } = 1.25f;
+    private RectTransform rectTransform;
+    private GameObject test;
+
+    void Start()
+    {
+        // Get the RectTransform component of the image
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        // Update the size of the image
+        rectTransform.localScale = new Vector3(size, size, 1f);
+    }
+}
